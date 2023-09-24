0ee2f527e464eb5b3893aad95fe24d3f84ffa507
Charlie Schmidt <cbronson@gmail.com>
2023-09-24 07:48:05 -0500

    refactored

--- loading.sh
+++ loading.sh
@@ -2,61 +2,59 @@
 
 # Function to check if a command exists
 command_exists() {
-    command -v $1 > /dev/null 2>&1
+    command -v "$1" > /dev/null 2>&1
 }
 
-# Check if Python 2 is installed
-if ! command -v python &> /dev/null; then
-    echo "Python 2 is not installed on your system."
-fi
-
-# Check if Python 3 is installed
-if ! command -v python3 &> /dev/null; then
-    echo "Python 3 is not installed on your system."
-fi
-
-# If neither Python 2 nor Python 3 is installed, prompt the user to install
-if [ ! "$(command -v python)" ] && [ ! "$(command -v python3)" ]; then
-    read -p "Do you want to install Python? (y/n): " choice
-    if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
-        # Install Python (adjust package manager and command as needed)
-        sudo apt-get install python
-        echo "Python has been installed successfully."
-    else
-        echo "Python is required for this script. Please install it manually."
-    fi
-else
-    echo "Python is already installed on your system."
-fi
-
+# Function to install Python
+install_python() {
+    sudo apt-get install python
+    echo "Python has been installed successfully."
+}
 
 # Function to display a welcome message
 display_welcome() {
+    local message="Welcome $USER"
+
     if command_exists figlet; then
-        figlet -f mini "W e l c o m e   $USER" | while IFS= read -r line; do
+        figlet -f mini "$message" | while IFS= read -r line; do
             echo -e "\033[2K\033[1G$line"
             sleep 0.1  # Adjust the sleep duration to control the animation speed
         done
     else
-        echo "Welcome $USER"
+        echo "$message"
     fi
 }
 
 # Main function
 main() {
-    # Color definitions
-    red=$(tput setaf 1)
-    green=$(tput setaf 2)
-    yellow=$(tput setaf 3)
-    blue=$(tput setaf 4)
-    magenta=$(tput setaf 5)
-    cyan=$(tput setaf 6)
-    white=$(tput setaf 7)
-    reset=$(tput sgr0)
+    # Check if Python 2 is installed
+    if ! command_exists python; then
+        echo "Python 2 is not installed on your system."
+        read -p "Do you want to install Python? (y/n): " choice
+        if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+            install_python
+        else
+            echo "Python is required for this script. Please install it manually."
+        fi
+    else
+        echo "Python 2 is already installed on your system."
+    fi
 
-    printf "${yellow}-------------------------------\n${green}"
+    # Check if Python 3 is installed
+    if ! command_exists python3; then
+        echo "Python 3 is not installed on your system."
+        read -p "Do you want to install Python 3? (y/n): " choice
+        if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+            install_python
+        else
+            echo "Python 3 is required for this script. Please install it manually."
+        fi
+    else
+        echo "Python 3 is already installed on your system."
+    fi
+
+    # Display welcome message
     display_welcome
-
 }
 
 # Run the main function
